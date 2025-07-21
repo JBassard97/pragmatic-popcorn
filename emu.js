c75b69fa3d97a65408736c556355900125f9c0d3
Jbassard <jonathanacciarito@gmail.com>
2025-07-21 19:48:21 -0400

    added a button resize slider

--- emu.js
+++ emu.js
@@ -2,6 +2,7 @@ const showOrHideSettings = (emu) => {
   switch (emu) {
     case "NES":
       document.querySelector(".nes-settings").style.display = "inline-block";
+      document.getElementById("controller").style.display = "flex";
       document.querySelector(".controller-row-1").style.display = "none";
       document.getElementById("screen").style.display = "inline-block";
       document.querySelector(".ds-container").style.display = "none";
@@ -9,6 +10,7 @@ const showOrHideSettings = (emu) => {
     case "GBA":
       document.querySelector(".nes-settings").style.display = "none";
       document.querySelector(".controller-row-1").style.display = "flex";
+      document.getElementById("controller").style.display = "flex";
       document.getElementById("screen").style.display = "inline-block";
       document.querySelector(".ds-container").style.display = "none";
       break;
